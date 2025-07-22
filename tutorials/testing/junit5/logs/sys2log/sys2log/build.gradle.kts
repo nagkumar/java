7e319c2eaf355789ad7fd1d3d962cd53b472aa2f
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2025-07-22 10:35:17 +0530

    ee

--- tutorials/testing/junit5/logs/sys2log/sys2log/build.gradle.kts
+++ tutorials/testing/junit5/logs/sys2log/sys2log/build.gradle.kts
@@ -27,14 +27,14 @@ sourceSets {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("org.slf4j:slf4j-api:2.1.0-alpha1")
     implementation("org.apache.logging.log4j:log4j-core:2.25.1")
     implementation("org.apache.logging.log4j:log4j-api:2.25.1")
     implementation("org.apache.logging.log4j:log4j-iostreams:2.25.1")
 
     //intellij does not support testImplementation for the test files within main folder, hence as work
     //arround making the test jars part of main
-    implementation(platform("org.junit:junit-bom:5.11.0"))
+    implementation(platform("org.junit:junit-bom:6.0.0-M1"))
     implementation("org.junit.jupiter:junit-jupiter")
     implementation("org.junit.platform:junit-platform-launcher")
     implementation("com.github.stefanbirkner:system-lambda:1.2.1")
