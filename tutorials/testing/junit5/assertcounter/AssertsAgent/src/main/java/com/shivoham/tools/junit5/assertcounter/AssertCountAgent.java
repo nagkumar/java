a39620c5c2a43dd3ee04096d99a4c21d21b1183b
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2025-07-22 12:09:02 +0530

    ee

--- tutorials/testing/junit5/assertcounter/AssertsAgent/src/main/java/com/shivoham/tools/junit5/assertcounter/AssertCountAgent.java
+++ tutorials/testing/junit5/assertcounter/AssertsAgent/src/main/java/com/shivoham/tools/junit5/assertcounter/AssertCountAgent.java
@@ -10,21 +10,20 @@ public final class AssertCountAgent
 {
     public static void premain(final String aAgentArgs, final Instrumentation aInstrumentation)
     {
-	new AgentBuilder.Default()
-		.type(ElementMatchers.nameEndsWith("Assertions")
-				     .or(ElementMatchers.nameContainsIgnoreCase("Assert"))
-				     .or(ElementMatchers.nameStartsWith("org.assertj"))
-				     .or(ElementMatchers.nameStartsWith("org.junit"))
-				     .or(ElementMatchers.nameStartsWith("org.hamcrest")))
-		.transform((builder,
-			    typeDescription,
-			    classLoader,
-			    javaModule,
-			    protectionDomain) ->
-				   builder.method(ElementMatchers.nameStartsWith("assert"))
-					  .intercept(MethodDelegation.to(AssertCounterInterceptor.class))
-			  )
-		.installOn(aInstrumentation);
+	new AgentBuilder.Default().type(ElementMatchers.nameEndsWith("Assertions")
+						       .or(ElementMatchers.nameContainsIgnoreCase("Assert"))
+						       .or(ElementMatchers.nameStartsWith("org.assertj"))
+						       .or(ElementMatchers.nameStartsWith("org.junit"))
+						       .or(ElementMatchers.nameStartsWith("org.hamcrest")))
+				  .transform((bBuilder,
+					      bTypeDefinitions,
+					      bClassLoader,
+					      bJavaModule,
+					      aProtectionDomain) ->
+						     bBuilder.method(ElementMatchers.nameStartsWith("assert"))
+							     .intercept(MethodDelegation.to(
+								     AssertCounterInterceptor.class)))
+				  .installOn(aInstrumentation);
 
 	Runtime.getRuntime().addShutdownHook(new Thread(AssertCounterInterceptor::printAssertsCountReport));
     }
