a21a1e8448ae82ebf5fb35db0060810e0b182774
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2025-07-25 22:08:55 +0530

    ee

--- tutorials/testing/junit5/assertcounter/build.gradle.kts
+++ tutorials/testing/junit5/assertcounter/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     `kotlin-dsl` apply false
 }
 
-subprojects { //Set group and version for both AssertsAgent and AssertsCounterPlugin.
+subprojects {
+
+    //Set group and version for both AssertsAgent and AssertsCounterPlugin.
     //Now you only have to change the version in this one file.
     group = "com.shivoham.tools.junit5.assertcounter"
     version = "1.0.8-SNAPSHOT"
