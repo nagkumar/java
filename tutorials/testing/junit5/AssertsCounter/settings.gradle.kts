30781f39c5f9fd27fae66588d3387c513ba281a6
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2025-07-26 14:32:46 +0530

    ee

--- tutorials/testing/junit5/AssertsCounter/settings.gradle.kts
+++ tutorials/testing/junit5/AssertsCounter/settings.gradle.kts
@@ -4,4 +4,4 @@ include("ACAgent")
 //include("ACPlugin")
 
 project(":ACAgent").name = "asserts-counter-agent"
-//project(":ACPlugin").name = "asserts-counter-plugin"
+project(":ACPlugin").name = "asserts-counter-plugin"
