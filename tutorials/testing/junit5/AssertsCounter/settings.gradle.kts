aff1b29f03cce39c6fe6faf5a69829b885c2677c
nagkumar <nagendra.raja@tejasoft.com>
2025-07-28 22:01:59 +0530

    wewe

--- tutorials/testing/junit5/AssertsCounter/settings.gradle.kts
+++ tutorials/testing/junit5/AssertsCounter/settings.gradle.kts
@@ -20,3 +20,14 @@ pluginManagement {
 	mavenCentral()
     }
 }
+
+plugins {
+    id("org.ajoberstar.reckon.settings") version "0.19.2"
+}
+
+extensions.configure<org.ajoberstar.reckon.gradle.ReckonExtension> {
+    setDefaultInferredScope("patch")
+    stages("beta", "rc", "final")
+    setScopeCalc(calcScopeFromProp().or(calcScopeFromCommitMessages()))
+    setStageCalc(calcStageFromProp())
+}
