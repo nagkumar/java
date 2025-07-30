5526cd426189d38bba7afd2edbad955c80db75f9
nagkumar <nagendra.raja@tejasoft.com>
2025-07-30 12:46:49 +0530

    removed redendant dependencies

--- tutorials/testing/junit5/AssertsCounter/build.gradle.kts
+++ tutorials/testing/junit5/AssertsCounter/build.gradle.kts
@@ -76,12 +76,11 @@ subprojects {
 	repositories {
 	    githubPackages()
 	}
+
 	publications {
 	    create<MavenPublication>("mavenJava") {
 		from(components["java"])
-		groupId = project.group.toString()
 		artifactId = project.name
-		version = project.version.toString()
 	    }
 	}
     }
