265c999b723e311e76eccbd7a0c6c73b57da2c38
nagkumar <nagendra.raja@tejasoft.com>
2025-07-31 20:33:43 +0530

    ee

--- tutorials/testing/junit5/AssertsCounter/build.gradle.kts
+++ tutorials/testing/junit5/AssertsCounter/build.gradle.kts
@@ -3,6 +3,10 @@ import fix.eu.davidea.gradle.VersioningExtension
 plugins {
     `java-library`
     `maven-publish`
+
+    id("com.github.ben-manes.versions") version ("0.52.0")
+    id("se.patrikerdes.use-latest-versions") version ("0.2.18")
+
     id("fix.eu.davidea.grabver") version "2.0.3"
 }
 
