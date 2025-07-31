c51df0f2a796ad3936468a42d8fca455caa6f5b0
nagkumar <nagendra.raja@tejasoft.com>
2025-07-31 07:58:27 +0530

    ee

--- tutorials/testing/junit5/AssertsCounter/ACPlugin/src/com/shivohamai/testing/tools/junit5/assertscounter/plugin/gradle/AddJarsAutoUpdatePlugins.kt
+++ tutorials/testing/junit5/AssertsCounter/ACPlugin/src/com/shivohamai/testing/tools/junit5/assertscounter/plugin/gradle/AddJarsAutoUpdatePlugins.kt
@@ -5,24 +5,18 @@ import org.gradle.api.Project
 
 class AddJarsAutoUpdatePlugins : Plugin<Project>
 {
-    companion object
-    {
-	const val VERSIONS_PLUGIN_VERSION = "0.52.0"
-	const val USE_LATEST_VERSIONS_PLUGIN_VERSION = "0.2.18"
-    }
-
     override fun apply(aProject: Project)
     {
 	aProject.pluginManager.apply("com.github.ben-manes.versions")
 	aProject.pluginManager.apply("se.patrikerdes.use-latest-versions")
 
 	aProject.afterEvaluate {
-	    println(" Jars auto update plugins applied successfully with versions:")
-	    println("- ben-manes.versions: $VERSIONS_PLUGIN_VERSION")
-	    println("- use-latest-versions: $USE_LATEST_VERSIONS_PLUGIN_VERSION")
-	    println("Available tasks:")
-	    println("- ./gradlew dependencyUpdates")
-	    println("- ./gradlew useLatestVersions")
+	    println("These below 2 Plugins are added to auto update jars")
+	    println("    1. ben-manes.versions")
+	    println("    2. use-latest-versions")
+	    println("So the available tasks are:")
+	    println("- ./gradle dependencyUpdates")
+	    println("- ./gradle useLatestVersions")
 	}
     }
 }
